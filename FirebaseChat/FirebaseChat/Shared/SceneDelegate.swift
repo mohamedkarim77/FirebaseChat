2fdfa706d5b3d90711ab6005c6ccf6ccd2c90b94
Mohamed Abdelkarim <d.mhmdkarim@gmail.com>
2022-07-14 19:37:50 +0400

    basic groups

--- FirebaseChat/FirebaseChat/Shared/SceneDelegate.swift
+++ FirebaseChat/FirebaseChat/Shared/SceneDelegate.swift
@@ -13,10 +13,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        configureAppStart(scene: scene)
+    }
+    
+    func configureAppStart(scene: UIWindowScene) {
+        let loginVC = LoginViewController()
+        let navigation = UINavigationController(rootViewController: loginVC)
+       // let mainTabBarVC = MainTabBarViewController()
+       // let customerId = UserDefaults.standard.string(forKey: Constants.CUSTOMER_ID)
+       // let controller = (customerId == "" || customerId == nil) ? navigation : mainTabBarVC
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = loginVC
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
